8fa720b736cad813ebb469a04df7015a4087a146
sollniss <sollniss@web.de>
2024-10-24 00:57:58 +0900

    feat: add codelens

--- extension.js
+++ extension.js
@@ -5,75 +5,87 @@ const vscode = require('vscode');
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
-let myStatusBarItem = null;
-
-const map = {
-	"asdfg": "test str"
-};
+let statusBarItem = null;
+let map = null;
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "strinfo" is now active!');
+	updateConfig();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('strinfo.helloWorld', async function () {
-		// The code you place here will be executed every time your command is executed
+	// Create status bar item.
+	statusBarItem = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right,
+		10000
+	);
 
-		myStatusBarItem = vscode.window.createStatusBarItem(
-			vscode.StatusBarAlignment.Right,
-			10000
+	context.subscriptions.push(statusBarItem);
+
+	// Update out map on configuration changes.
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+		if (e.affectsConfiguration("strinfo.mappings")) {
+			updateConfig();
+		}
+	}));
+
+	// Handle text highlight.
+	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(e => {
+		if (!map) {
+			statusBarItem.hide();
+			return
+		}
+
+		if (!e.selections) {
+			statusBarItem.hide();
+			return;
+		}
+		if (e.selections.length === 0) {
+			statusBarItem.hide();
+			return;
+		}
+		if (!e.textEditor) {
+			statusBarItem.hide();
+			return;
+		}
+		const selection = e.selections[0];
+
+		if (!selection || selection.isEmpty) {
+			statusBarItem.hide();
+			return;
+		}
+
+		// Not sure if this is required, getText also accepts a selection object.
+		const selectionRange = new vscode.Range(
+			selection.start.line, 
+			selection.start.character, 
+			selection.end.line, 
+			selection.end.character,
 		);
+		const text = e.textEditor.document.getText(selectionRange);
+		
+		if (map[text] === undefined) {
+			statusBarItem.hide();
+			return;
+		}
+		
+		statusBarItem.text = map[text]
+		statusBarItem.show();
+	}));
 
-		myStatusBarItem.show();
-		context.subscriptions.push(myStatusBarItem);
+	// Code lense
+	vscode.languages.registerCodeLensProvider("*", mappingLense);
+}
 
-		vscode.window.onDidChangeTextEditorSelection(e => {
-			if (!e.selections) {
-				myStatusBarItem.hide();
-				return;
-			}
-			if (e.selections.length === 0) {
-				myStatusBarItem.hide();
-				return;
-			}
-			if (!e.textEditor) {
-				myStatusBarItem.hide();
-				return;
-			}
-			const selection = e.selections[0];
+function updateConfig() {
+	map = vscode.workspace.getConfiguration("strinfo.mappings");
 
-			if (!selection || selection.isEmpty) {
-				myStatusBarItem.hide();
-				return;
-			}
-
-			const selectionRange = new vscode.Range(
-				selection.start.line, 
-				selection.start.character, 
-				selection.end.line, 
-				selection.end.character,
-			);
-			const text = e.textEditor.document.getText(selectionRange);
-			
-			if (map[text] === undefined) {
-				myStatusBarItem.hide();
-				return;
-			}
-			
-			myStatusBarItem.text = map[text]
-			myStatusBarItem.show();
-		 });
-
-	});
-
-	context.subscriptions.push(disposable);
+	let s = ""
+	for (const [key, value] of Object.entries(map)) {
+		s += key + "|"
+	}
+	regexStr = s.substring(0, s.length - 1);
 }
 
 // This method is called when your extension is deactivated
@@ -82,4 +94,32 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+
+let regexStr = null;
+const mappingLense = {
+  provideCodeLenses: function (document, token) {
+		let codeLenses = [];
+		const regex = new RegExp(regexStr, 'g');
+		const text = document.getText();
+		let matches;
+		while ((matches = regex.exec(text)) !== null) {
+			const line = document.lineAt(document.positionAt(matches.index).line);
+			const indexOf = line.text.indexOf(matches[0]);
+			const position = new vscode.Position(line.lineNumber, indexOf);
+			const range = document.getWordRangeAtPosition(position, new RegExp(regexStr, 'g'));
+			if (range) {
+				codeLenses.push(new vscode.CodeLens(range, {
+					command: "workbench.action.findInFiles",
+					arguments: [
+						{
+							query: matches[0],
+						},
+					],
+					title: map[matches[0]],
+				}));
+			}
+		}
+		return codeLenses;
+  }
 }
